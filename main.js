1399ed04d5381f6cb8e392bc42561966bcf3f8c1
KaverzaCyberSchizo <zarnikei@gmail.com>
2023-12-07 18:01:51 +0300

    lesson 3 - start

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-document
-.getElementById('output')
-.innerText = 'Welcome'
+//document
+//.getElementById('output')
+//.innerText = 'Welcome';
+
+const domOutput = document.getElementById('output');
+const domInput = document.getElementById('input');
+const domButton = document.getElementById('enter');
+
+// domOInput.value = '123';
+// domOutput.innerText = 'Welcome';
+
+domInput.oninput = function(event) {
+  console.log('oninput', event);
+  // domOutput.innerText = 'Hello, ' + event.target.value;
+}
+
+domButton.onclick = function(event) {
+  console.log('onclick', event);
+  domOutput.innerHTML = domOutput.innerHTML + `<div>${domInput.value}</div>`;
+}

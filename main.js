ff6bcbb90a719146504d63a6ece0872098c2b1c6
KaverzaCyberSchizo <zarnikei@gmail.com>
2023-12-07 18:27:12 +0300

    lesson 4 - start

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-//document
-//.getElementById('output')
-//.innerText = 'Welcome';
-
-const domOutput = document.getElementById('output');
-const domInput = document.getElementById('input');
-const domButton = document.getElementById('enter');
-
-// domOInput.value = '123';
-// domOutput.innerText = 'Welcome';
-
-domInput.oninput = function(event) {
-  console.log('oninput', event);
-  // domOutput.innerText = 'Hello, ' + event.target.value;
 }
 
-domButton.onclick = function(event) {
-  console.log('onclick', event);
-  domOutput.innerHTML = domOutput.innerHTML + `<div>${domInput.value}</div>`;
+onst domOutput = document.getElementById('output'); 
+const domInput = document.getElementById('input'); 
+const domButton = document.getElementById('enter'); 
+ 
+domButton.onclick = function(event) { 
+  console.log('domButton -> onclick: 0) - check input is not empty'); 
+  if (domInput.value.length > 0) { 
+    console.log('domButton -> onclick: 1) - add todo'); 
+    domOutput.innerHTML += `<div>${domInput.value}</div>`; 
+    console.log('domButton -> onclick: 2) - clear input'); 
+    domInput.value = ''; 
+  } else { 
+    domInput.style.borderColor = "red" 
+    // alert('Wrong input'); 
+  } 
 }

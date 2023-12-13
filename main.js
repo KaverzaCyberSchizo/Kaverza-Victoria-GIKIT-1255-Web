82d37e7987b0d688de34fe043cb7751118a5afd8
KaverzaCyberSchizo <zarnikei@gmail.com>
2023-12-13 17:12:43 +0300

    lesson 5 - finish, delete todo

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const domButton = document.getElementById('enter');
 
 let numberOfTodos = 0;
 
+// listener methods
+
 const onCreateTodoButtonClick = function (event) {
   console.log('domButton -> onclick: 0) - check input is not empty');
   if (checkInputWithText()) {
@@ -22,6 +24,11 @@ const onInputTextInput = function (event) {
 }
 
 const checkInputWithText = () => domInput.value.length > 0;
+
+const onTodoDeleteClick = function(event) { 
+  console.log('todoDelete -> onTodoDeleteClick', event);
+  event.currentTarget.parentNode.remove(); 
+}
 // render methods
 
 const renderAlert = function () {
@@ -30,13 +37,35 @@ const renderAlert = function () {
 }
 const renderInputTextInOutout = function () {
   console.log('domButton -> onclick: 1) - add todo');
-  domOutput.innerHTML += `<div>${numberOfTodos}. ${domInput.value} <button style="background-color:red;" onclick="onTodoDeleteClick">x</button>
-  </div>`;
+  const domTodoContainer = document.createElement('div'); 
+  
+   const domTodoButtonDelete = createActionButtonWithText ('delete')
+   domTodoButtonDelete.onclick = onTodoDeleteClick;
+ 
+  domTodoContainer.innerText = `${numberOfTodos}. ${domInput.value}`; 
+ 
+  domTodoContainer.appendChild(domTodoButtonDelete); 
+  domOutput.appendChild(domTodoContainer);
   console.log('domButton -> onclick: 2) - clear input');
   domInput.value = '';
 }
 const renderInputHighLightColor = (color) => domInput.style.backgroundColor = color;
+
 // Render ends
+
+const createActionButtonWithText = (text) => {
+  const btn = document.createElement('button'); 
+  btn.innerText = text; 
+  btn.style.backgroundColor = 'red'; 
+  btn.style.marginLeft = '12px'; 
+  btn.style.border = 'solid 2px'; 
+  btn.style.borderColor = 'green'; 
+  return btn;
+}
 
 domButton.onclick = onCreateTodoButtonClick;
 domInput.oninput = onInputTextInput;
+
+domInput.value = "Test"; 
+numberOfTodos = 1; 
+renderInputTextInOutput();
